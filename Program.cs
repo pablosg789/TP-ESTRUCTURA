933de702093196cc931c42b7e348961d43668ef4
pablosg789 <42720199+pablosg789@users.noreply.github.com>
2018-11-05 01:04:58 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,139 +1,262 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TrabajoPractico
+
+namespace tpEstructura
 {
     class Program
     {
         static void Main(string[] args)
         {
+            SortedList menu = new SortedList(); // ------------Creamos un Diccionario con los elementos del menu
+            menu.Add("MILANESA", 180.0f);
+            menu.Add("POLLO", 180.0f);
+            menu.Add("RAVIOLES", 200.0f);
+            menu.Add("SORRENTINOS", 200.0f);
+            menu.Add("ASADO", 220.0f);
+            menu.Add("GASEOSA", 50.0f);
+            menu.Add("AGUA", 50.0f);
+            menu.Add("CERVEZA", 100.0f);
+            menu.Add("VINO", 200.0f);
+            menu.Add("TRAGO", 90.0f);
+
+            ArrayList factura = new ArrayList();   // -------- creamos una lista para almacenar la seleccion de alimentos
+            ArrayList montos = new ArrayList();    // -------- creamos otra lista para almacenar los valores de los precios
+
+            Console.WriteLine("\n***********************PEDIDO***********************" +
+                            "\n\nINGRESE EL NúMERO DE LA OPCIÓN QUE DESEA AGREGAR : "); 
+            MostrarMenu(menu);  // --------------------------- MUESTRA LAS OPCIONES DE MENU - RECORRIENDO UN DICCIONARIO
+            string pedido = Pedido(PedirOpcion()); // -------- PIDE UNA OPCION AL USUARIO Y LA GUARDA EN LA VARIABLE PEDIDO 
+            factura.Add(pedido); // -------------------------- LA OPCION DEL USUARIO SE GUARDA EN UNA LISTA (LA CLAVE DE DICCIONARIO EJEMPLO "MILANESA")
+            float precio = Convert.ToSingle(menu[pedido]);// - GUARDAMOS EL VALOR DE LA CLAVE DEL DICCIONARIO (EL PRECIO ) QUE ELIJIO EL USUARIO
+            montos.Add(precio);// ---------------------------- EL PRECIO SE GUARDA EN OTRA LISTA
+            
+            Limpiar(); // -- limpia la consola
+            float cuenta = precio;
+
+            bool fin = true;
+
+
+            ImprimirOpciones();
+            while(fin == true)
+
+            {
+                
+                string opciones = Opciones(PedirOpcion());
+                Limpiar();
+                
+                if (opciones == "AGREGAR")  
+                {
+                    Limpiar();
+                    Console.WriteLine("\nAgregar una opción al pedido\n"); 
+                    MostrarMenu(menu);//------------------------- SE MUESTRAN LAS OPCIONES DEL MENU
+                    pedido = Pedido(PedirOpcion());
+                    precio = Convert.ToSingle(menu[pedido]);
+                    factura.Add(pedido);// ------------------------ SE AGREGA A LA LISTA FACTURA - LA OPCION SELECCIONADA
+                    int indice = factura.IndexOf(pedido);
+                    montos.Add(precio);//-------------------------- SE AGREGA A LA LISTA MONTOS - EL PRECIO DE LA OPCION SELECCIONADA
+                    cuenta = Agregar(cuenta, precio);//------------ SE LE SUMA EL PRECIO A LA VARIABLE CUENTA
+                    
+                }
+                else if (opciones == "QUITAR")
+                {
+                    Limpiar();
+                    
+                    
+                    Console.WriteLine("\nQuitar una opción al pedido\n");
+                    MostrarPedidoParcial(factura); // MUESTRA LO AGREGADO EN LA LISTA DE COMIAS
+                    Console.WriteLine("");
+                    int opcionEliminar = Convert.ToInt32(Console.ReadLine()); // PIDE OPCION AL USUARIO
+                    float descontar = Convert.ToSingle(montos[opcionEliminar]); 
+                    factura.RemoveAt(opcionEliminar); // ELIMINA LA OPCION DE LA LISTA DE COMIDAS/BEBIDAS QUE INGRESO EL USUARIO
+                    montos.RemoveAt(opcionEliminar); // ELIMINA LA OPCION DE LA LISTA DE PRECIOS QUE INGRESO EL USUARIO
+                    cuenta = Quitar(cuenta, descontar);  // SE LE RESTA EL PRECIO DE LA OPCION ELIMINADA A LA VARIABLE CUENTA
+                    
+                }
+                else if (opciones == "MOSTRAR PEDIDO")
+                {
+                    Limpiar();
+
+                    MostrarPedidoParcial(factura);
 
 
 
-            // creamos un arreglo con las opciones de comidas del menu
-            string[] comidas = { "Milanesas           $ 180.-", "Ravioles            $ 200.-", "Sorrentinos         $ 200.-",
-                "Pollo               $ 180.-", "Asado               $ 220.-" };
-            // creamos un arreglo con las opciones de bebidas del menu
-            string[] bebidas = { "Cerveza             $ 100.-", "Vino                $ 200.-", "Gaseosa             $ 50.-", "Agua                $ 50.-",
-                "Trago              $ 90.-" };
-            // creamos un arreglo con precios
-            float[] precio = new float[10];
 
-            precio[0] = 180;// milanesas, pollo
-            precio[1] = 200;// ravioles, sorrentinos
-            precio[2] = 220;// asado
-            precio[3] = 60;// papas
-            precio[4] = 50;// gaseosa agua
-            precio[5] = 100;// cerveza 
-            precio[6] = 200;// vino
-            precio[7] = 90;// trago         
+                }
+                else if(opciones == "SUBTOTAL")
+                {
+                    Console.WriteLine($"\nSUBTOTAL : ${cuenta}");
+                                       
+
+                }
+                else if (opciones == "DESCUENTO")
+                {
+                    cuenta = Descuento(ref cuenta);
 
 
+                }
+                else if(opciones == "IMPRIMIR CUENTA")
+                {
+                    Console.WriteLine("\n\n****************DETALLE****************\n");
+                    ImprimirFactura(factura,montos);
+                    Console.WriteLine("\nTOTAL ..............$ {0}.-\n\n\n",cuenta);
+                    
+                    fin = false;
+                    if (fin == false) { break; }
+                }
+                
+                Console.WriteLine("");
+                ImprimirOpciones();
+                
+
+            }
+
+            
 
 
-            //cantidad de elementos de los arreglos
-            byte opcionesDeComidas = Convert.ToByte(comidas.Length), opcionesDeBebidas = Convert.ToByte(bebidas.Length);
+        }//-----------------------------------------------------------------------------------------Main
 
-            //Mostramos el menu
-            Console.WriteLine("MENÚ\n");
-            Console.WriteLine("COMIDAS: \n");
-            MostrarMenuComida(opcionesDeComidas, comidas);
-
-
-
-
-            Console.WriteLine("");
-            Console.WriteLine("\nBEBIDAS: \n");
-            MostrarMenuBebida(bebidas);
-            Console.WriteLine("");
-            Console.WriteLine(Pedido(precio));
-            Console.WriteLine(Pedido(precio));
-
-
-
-
-        }//----------------Cierre de Main
-
-
-        static float Pedido(float[] pedido)
+               
+        static int PedirOpcion()
         {
-            Console.Write("Ingresar opcion: ");
-            byte cliente = Convert.ToByte(Console.ReadLine());
-            float opcion = 0;
-            switch (cliente)
+            Console.WriteLine("");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }//-----------------------------------------------------------------------------------------PedirOpcion
+
+        
+        static float Descuento(ref float valor)
+        {
+            Console.Write("Ingresar porcentaje de descuento :   %  ");
+            float porcentaje = Convert.ToSingle(Console.ReadLine());
+            porcentaje = porcentaje / 100;
+            float resultado = valor * porcentaje;
+            float valorFinal = valor - resultado;
+            return valorFinal;
+
+            
+        }//-----------------------------------------------------------------------------------------Descuento
+
+
+        static void MostrarMenu(SortedList diccionario) // Recorre el diccionario con un for
+        {
+            Console.WriteLine("");
+            for (int i = 0, x = 1; i < diccionario.Count; i++, x++) { Console.WriteLine("{1}. {0}", diccionario.GetKey(i), x); }
+        }//------------------------------------------------------------------------------------------MostrarMenu
+
+
+        static string Pedido(int opcion)
+        {
+            switch (opcion)
             {
                 case 1:
-                    Console.Write("Milanesas: $");
-                    opcion = pedido[0]; // milanesa
-                    return opcion;
-
+                    return "AGUA";
                 case 2:
-                    Console.Write("Ravioles: $");
-                    opcion = pedido[1];//ravioles
-                    return opcion;
-
+                    return "ASADO";
                 case 3:
-                    Console.Write("Sorrentinos: $");
-                    opcion = pedido[1];//sorrentinos
-                    return opcion;
-
+                    return "CERVEZA";
                 case 4:
-                    Console.Write("Pollo: $");
-                    opcion = pedido[0];//pollo
-                    return opcion;
-
+                    return "GASEOSA";
                 case 5:
-                    Console.Write("Asado: $");
-                    opcion = pedido[2];//asado
-                    return opcion;
+                    return "MILANESA";
                 case 6:
-                    Console.Write("Cerveza: $");
-                    opcion = pedido[5];//cerveza
-                    return opcion;
+                    return "POLLO";
                 case 7:
-                    Console.Write("Vino: $");
-                    opcion = pedido[6];//vino 
-                    return opcion;
+                    return "RAVIOLES";
                 case 8:
-                    Console.Write("Gaseosa: $");
-                    opcion = pedido[4];//gaseosa
-                    return opcion;
+                    return "SORRENTINOS";
                 case 9:
-                    Console.Write("Agua: $");
-                    opcion = pedido[4];//agua
-                    return opcion;
+                    return "TRAGO";
                 case 10:
-                    Console.Write("Trago: $");
-                    opcion = pedido[7];//trago
-                    return opcion;
-
+                    return "VINO";
                 default:
-                    return 0;
+                    return "ERROR";
 
             }
-        }//-----------------Cierre Pedido
+        }//--------------------------------------------------------------------------------------------Pedido
 
-
-        static void MostrarMenuComida(byte cantidadElementosDelArreglo, string[] arreglo)
+        static void ImprimirOpciones()
         {
-            for (byte i = 1, x = 0; i <= cantidadElementosDelArreglo; i++, x++)
-            {
-                Console.WriteLine($"{i}. {arreglo[x]}");
+            Console.WriteLine("\n1. AGREGAR\n2. QUITAR\n3. MOSTRAR PEDIDO\n4. SUBTOTAL\n5. DESCUENTO\n6. IMPRIMIR CUENTA");
+        }
 
-            }
-        }//-----------------Cierre MostrarMenuComida
-        static void MostrarMenuBebida(string[] arreglo)
+
+        static string Opciones(int opcion)
         {
-            for (byte i = 6, x = 0; i <= 10; i++, x++)
+            
+
+            switch (opcion)
             {
-                Console.WriteLine($"{i}. {arreglo[x]}");
+                case 1:
+                    return "AGREGAR";
+                case 2:
+                    return "QUITAR";
+                case 3:
+                    return "MOSTRAR PEDIDO";
+                case 4:
+                    return "SUBTOTAL";
+                case 5:
+                    return "DESCUENTO";
+                case 6:
+                    return "IMPRIMIR CUENTA";
+                
+                default:
+                    return "ERROR";
 
             }
-        }//-----------------Cierre MostrarMenuBebida
+        }//--------------------------------------------------------------------------------------------Opciones
 
 
+            static float Agregar(float valor1, float valor2)
+        {
+            return valor1 + valor2;
+        }//---------------------------------------------------------------------------------------------Agregar
+
+
+
+
+        static float Quitar(float valor1, float valor2)
+        {
+            return valor1 - valor2;
+        }//---------------------------------------------------------------------------------------------Quitar
+
+        static void Limpiar()
+        {
+            Console.Clear();
+        }//-----------------------------------------------------------------------------------------------Limpiar
+        static void MostrarPedidoParcial(ArrayList factura)
+        {
+            for (byte i = 0; i < Convert.ToByte(factura.Count); i++)
+            {
+                
+                Console.WriteLine("{1}. {0}", factura[i], factura.IndexOf(factura[i]));
+
+            }
+
+        }//-----------------------------------------------------------------------------------------------MostrarPedidoParcial
+
+
+        static void ImprimirFactura(ArrayList factura, ArrayList montos)
+        {
+            for (byte i = 0,  x = 0; i < Convert.ToByte(montos.Count); i++, x++)
+            {
+                
+                Console.WriteLine("{0} ............$ {1}.-",Convert.ToString(factura[i]),Convert.ToSingle(montos[x]));
+                
+            }
+            
+
+
+        }//--------------------------------------------------------------------------------------------ImprimirFactura
+        
+
+        
+
+
+      
     }
 }
